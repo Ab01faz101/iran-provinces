891b9ffd1841773855d7ad2fa6551f9e8339308c
reza <ar.rahmatyy@gmail.com>
2026-09-12 13:17:03 -0700

    Initial commit

---
+++ src/Models/City.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ab01faz101\IranProvinces\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class City extends Model
+{
+    protected $fillable = [
+        'province_id',
+        'name',
+    ];
+
+    public function province(): BelongsTo
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+}
